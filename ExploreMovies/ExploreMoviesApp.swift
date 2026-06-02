5dda92a96555ac89d724d9d0b730432345aff8c3
henriquepablo <pablo269704@gmail.com>
2026-06-02 20:34:13 -0300

    Initial Commit

---
+++ ExploreMovies/ExploreMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreMoviesApp.swift
+//  ExploreMovies
+//
+//  Created by pablo henrique on 02/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ExploreMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExploreMoviesTests/ExploreMoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreMoviesTests.swift
+//  ExploreMoviesTests
+//
+//  Created by pablo henrique on 02/06/26.
+//
+
+import Testing
+@testable import ExploreMovies
+
+struct ExploreMoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
