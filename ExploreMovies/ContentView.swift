59672829756601b7ccefb5735e4a54cea1078d93
henriquepablo <pablo269704@gmail.com>
2026-06-02 21:06:25 -0300

    chore: config colors and fonts

--- ExploreMovies/ContentView.swift
+++ ExploreMovies/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(.custom("Rajdhani-Bold", size: 22))
         }
         .padding()
     }
